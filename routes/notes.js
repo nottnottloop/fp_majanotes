94959708e08cb78d6c4fc190634af2ebc44ae4c1
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-03-04 19:48:53 +0000

    can POST to server, and save json file

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
 
 const note = require("../model/noteModel");
@@ -13,8 +14,28 @@ router
   res.render("notes/new");
   })
   .post((req, res) => {
+    //create a new note object, using the model, based on response we recieved from the user
     const newNote = new note(req.body.title, req.body.note, req.body.color);
-    console.log(`Title: ${newNote.title}\nNote: ${newNote.note}\nColor: ${newNote.color}\n`)
+    let notesJson;
+    let notesData;
+    try {
+      //open and read the notes json file we have stored
+      let notesJson = fs.readFileSync("data/notesData.json", "utf-8");
+      //parse the notesJson (file) into a notesData object that works natively in JS. we can't manipulate a json directly
+      notesData = JSON.parse(notesJson);
+    } catch (err) {
+      console.log("Error: " + err);
+      notesJson = [];
+      notesData = [];
+    }
+    //append our new note
+    notesData.push(newNote);
+    //turn our native JS object back into a json file, ready to write it back
+    notesJson = JSON.stringify(notesData);
+    //write the json file
+    fs.writeFileSync("data/notesData.json", notesJson, "utf-8");
+    console.log(`New note added:\nTitle: ${newNote.title}\nNote: ${newNote.note}\nColor: ${newNote.color}\n`)
+    //this whole route is mounted on /notes. so this redirect redirects to /notes, not /
     res.redirect(".");
   });
 
