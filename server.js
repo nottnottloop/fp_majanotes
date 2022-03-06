25b94f99384d157645452e9b8c3b1fbf336ef7f3
mwezn <m.w.g.nelson@gmail.com>
2022-03-06 15:59:25 +0000

    added data folder for json file

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const notesRouter = require("./routes/notes");
 const emojiRouter = require("./routes/emoji");
 
 app.use(notesRouter)
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
