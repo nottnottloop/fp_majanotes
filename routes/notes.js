8a92542545a1f517b682a072dd24b0ec2975cf0f
mwezn <m.w.g.nelson@gmail.com>
2022-03-06 19:12:36 +0000

    Put our JSON length inside ejs

--- routes/notes.js
+++ routes/notes.js
@@ -16,9 +16,11 @@ router.get("/", (req, res) => {
   try {
     //read the file, send it back
     notesJson = fs.readFileSync(path.resolve(__dirname, "../data/notesData.json"), "utf-8");
-    res.render("notes/index.ejs",{data: notesJson});
-  } catch (err) {
-    res.send([]);
+    console.log(notesJson,JSON.parse(notesJson))
+    res.render("notes/index.ejs",{data: JSON.parse(notesJson)})
+    
+  } catch(err){
+    res.send(err)
   }
   
 });
