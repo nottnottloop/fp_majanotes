8482f68cfc2114b98c6554fc51d0d1afe63d543b
mwezn <m.w.g.nelson@gmail.com>
2022-03-06 15:33:59 +0000

    Made title & notebox required fields

--- Test/api.test.js
+++ Test/api.test.js
@@ -3,16 +3,16 @@ const app = require("../server");
 const request= supertest(app)
 
 describe('api test', () => {
-    it('gets the cinemas endpoint', async ()=> {
-        const res = await request.get("/cinemas");
+    it('gets the new notes page', async ()=> {
+        const res = await request.get("/new");
         expect(res.statusCode).toBe(200)
     });
-    it('cant post to cinemas endpoint', async ()=>{
-        const res=await request.post("/cinemas")
-        expect(res.statusCode).toBe(404)
+    it('can post to new notes', async ()=>{
+        const res=await request.post("/new")
+        expect(res.statusCode).toBe(200)
     })
-    it('Create new park', async ()=>{
-        const res=await request.post("/parks/create")
-        expect(res.statusCode).toBe(400)
+    it('Gets homepage', async ()=>{
+        const res=await request.get("/")
+        expect(res.statusCode).toBe(200)
     })
 });
