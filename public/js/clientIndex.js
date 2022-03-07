1f25378846e7e7b6b74d500dd3129f8bfbd8a4de
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-03-07 16:04:14 +0000

    refactor client new note error code

--- public/js/clientIndex.js
+++ public/js/clientIndex.js
@@ -1,16 +1,19 @@
 const protocol = window.location.protocol;
 const host = window.location.host;
 
+const createButton = document.querySelector("#createButton");
+const newMajanote = document.querySelector("#newMajanote");
+const errorMessages = document.querySelector("#errorMessages");
+
+let errors;
+errorMessages.textContent.length > 0 ? errors = true : errors = false;
+
 fetch(`${protocol}//${host}/data`)
 	.then(resp => resp.json())
 	.then(data => renderNotes(data))
 	.catch(e => console.log(`Error: ${e}`));
 
-const createButton = document.querySelector("#createButton");
-const newMajanote = document.querySelector("#newMajanote");
-const errorMessages = document.querySelector("#errorMessages");
-
-if (errorMessages.textContent.length > 0) {
+if (errors) {
 	newMajanote.style.display = "initial";
 } else {
 	newMajanote.style.display = "none";
