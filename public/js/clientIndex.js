5e9fe4b2420dfd3ff533a3db79c1cba9c0476430
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-03-07 15:59:35 +0000

    errors show up on the page correctly

--- public/js/clientIndex.js
+++ public/js/clientIndex.js
@@ -6,9 +6,15 @@ fetch(`${protocol}//${host}/data`)
 	.then(data => renderNotes(data))
 	.catch(e => console.log(`Error: ${e}`));
 
-const newMajanote = document.querySelector("#newMajanote");
-newMajanote.style.display = "none";
 const createButton = document.querySelector("#createButton");
+const newMajanote = document.querySelector("#newMajanote");
+const errorMessages = document.querySelector("#errorMessages");
+
+if (errorMessages.textContent.length > 0) {
+	newMajanote.style.display = "initial";
+} else {
+	newMajanote.style.display = "none";
+}
 
 createButton.addEventListener('click', () => {
 	if (newMajanote.style.display === 'none') {
