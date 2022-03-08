fafe636129e63956d0231fe5855f4c98608b0164
mwezn <m.w.g.nelson@gmail.com>
2022-03-08 10:47:45 +0000

    created userModel.js

--- model/noteModel.js
+++ model/noteModel.js
@@ -7,12 +7,6 @@ class Note {
 	}
 }
 
-class User {
-	constructor(id,username,password){
-		this.id=id;
-		this.username=username;
-		this.password=password;
-	}
-}
 
-module.exports = {Note,User};
+
+module.exports = Note;
