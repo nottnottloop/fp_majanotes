73324ba4b1bf8457df89629bf3c368272b4a38ef
mwezn <m.w.g.nelson@gmail.com>
2022-03-08 15:10:05 +0000

    Adding fetch routes based on localStorage existence

--- public/js/clientBase.js
+++ public/js/clientBase.js
@@ -1,7 +1,6 @@
 window.addEventListener("pageshow", e => {
 	let historyTraversal;
 	window.performance.getEntriesByType("navigation")[0].type === "back_forward" ? historyTraversal = true : historyTraversal = false;
-	console.log(historyTraversal)
 	if (historyTraversal) {
 		// Handle page restore.
 		window.location.reload();
