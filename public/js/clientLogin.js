80c28d022f86721b050766c9d18e59ef8eb2e96e
mwezn <m.w.g.nelson@gmail.com>
2022-03-08 17:13:43 +0000

    Added clientLogin.js

--- public/js/clientLogin.js
+++ public/js/clientLogin.js
@@ -1,14 +1,6 @@
 const protocol = window.location.protocol;
 const host = window.location.host;
 
-const createButton = document.querySelector("#createButton");
-const newMajanote = document.querySelector("#newMajanote");
-const errorMessages = document.querySelector("#errorMessages");
-//const email=window.localStorage.getItem("User")
-//console.log(email)
-
-let errors;
-errorMessages.textContent.length > 0 ? errors = true : errors = false;
 
 let user= window.localStorage.getItem("User")
 
@@ -18,16 +10,5 @@ user?fetch(`${protocol}//${host}/data`)
 	.catch(e => console.log(`Error: ${e}`)):
 	fetch(`${protocol}//${host}/loggedout`)
 
-if (errors) {
-	newMajanote.style.display = "initial";
-} else {
-	newMajanote.style.display = "none";
-}
 
-createButton.addEventListener('click', () => {
-	if (newMajanote.style.display === 'none') {
-		newMajanote.style.display = "initial";
-	} else {
-		newMajanote.style.display = "none";
-	}
-});
+
